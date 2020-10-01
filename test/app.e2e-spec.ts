1a5f4040032948b9217721d14bd8b765be3afd17
Gustavo Baptista <gustavogsb@outlook.com>
2020-10-01 18:51:44 -0300

    Add nodejs ci

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,8 +2,10 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { response } from 'express';
+import { async } from 'rxjs';
 
-describe('AppController (e2e)', () => {
+describe('Products Controller (e2e)', async () => {
   let app: INestApplication;
 
   beforeEach(async () => {
@@ -15,10 +17,57 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  const product = {
+    name: 'Coca Cola',
+    weight: 3.1,
+    imageUrl: 'http://image-1.jpg',
+  };
+
+  const productNew = {
+    name: 'Soda',
+    weight: 2.75,
+    imageUrl: 'http://image-2.jpg',
+  };
+
+  let productId;
+
+  it('POST /product', async () => {
+    const res = await request(app.getHttpServer())
+      .post('/products')
+      .send(product)
+      .expect(201);
+
+    expect(res.body.name).toEqual(product.name);
+    expect(res.body.weight).toEqual(product.weight);
+    expect(res.body.imageUrl).toEqual(product.imageUrl);
+
+    productId = res.body._id;
+  });
+
+  it('GET /product/:id', async () => {
+    const res = await request(app.getHttpServer())
+      .get(`/products/${productId}`)
+      .expect(200);
+
+    expect(res.body.name).toEqual(product.name);
+    expect(res.body.weight).toEqual(product.weight);
+    expect(res.body.imageUrl).toEqual(product.imageUrl);
+  });
+
+  it('PUT /product/:id', async () => {
+    const res = await request(app.getHttpServer())
+      .put(`/products/${productId}`)
+      .send(productNew)
+      .expect(200);
+
+    expect(res.body.name).toEqual(productNew.name);
+    expect(res.body.weight).toEqual(productNew.weight);
+    expect(res.body.imageUrl).toEqual(productNew.imageUrl);
+  });
+
+  it('DELETE /product/:id', async () => {
+    await request(app.getHttpServer())
+      .delete(`/products/${productId}`)
+      .expect(200);
   });
 });
