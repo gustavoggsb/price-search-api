34f52f73b2fef12c486323f201bb0cd0af54cfcb
Gustavo Baptista <gustavogsb@outlook.com>
2020-10-09 18:10:56 -0300

    Remove @nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     ProductsModule,
-    ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.DATABASE_URL || 'mongodb://localhost:27017', { useFindAndModify: false}),
   ],
   controllers: [],
