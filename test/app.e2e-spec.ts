3b76462168089f0736e85e10976ae9a334b8d918
Gustavo Baptista <gustavogsb@outlook.com>
2020-10-12 19:01:24 -0300

    Validate array size

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -47,11 +47,13 @@ describe('ProductsController (e2e)', () => {
     const res = await request(app.getHttpServer())
       .get(`/products`)
       .expect(200);
+    console.log(res.body)
     
     expect(res.body[0].name).toEqual(product.name);
     expect(res.body[0].weight).toEqual(product.weight);
     expect(res.body[0].imageUrl).toEqual(product.imageUrl);
     expect(res.body[0]._id).toEqual(productId);
+    expect(res.body).toHaveLength(1)
   });
 
   it('GET /products/:id', async () => {
