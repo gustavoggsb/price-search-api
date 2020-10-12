fb0fd04a2cd09e2072303cf550e4cc259ed74850
Gustavo Gabriel <56235370+Gustavoggsb@users.noreply.github.com>
2020-10-12 16:30:20 -0300

    Deploy to heroku (#9)
    * Add procfile
    
    * Remove @nestjs/config
    
    * Fix CORS policy

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,9 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
-  const options = new DocumentBuilder()
-    .setTitle('Price Search API')
-    .build();
+  const options = new DocumentBuilder().setTitle('Price Search API').build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
